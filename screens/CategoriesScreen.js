03d67d7c27615e2c663945a343ccef2f5ac63cbe
arcanlte <pinangayjoel@gmail.com>
2020-06-14 12:02:07 +0800

    data folder created

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,12 +1,8 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, FlatList, StyleSheet } from "react-native";
 
 const CategoriesScreen = (props) => {
-  return (
-    <View style={styles.screen}>
-      <Text>The Categories Screen!</Text>
-    </View>
-  );
+  return <FlatList numColumns={2} />;
 };
 
 const styles = StyleSheet.create({
