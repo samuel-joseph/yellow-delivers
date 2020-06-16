e75925a14430b504fd38a6048c214c3b1b663799
arcanlte <pinangayjoel@gmail.com>
2020-06-16 08:25:17 +0800

    Hamburger menu created

--- navigation/MealsNavigation.js
+++ navigation/MealsNavigation.js
@@ -5,10 +5,12 @@ import { createBottomTabNavigator } from "react-navigation-tabs";
 import { createAppContainer } from "react-navigation";
 import { Ionicons } from "@expo/vector-icons";
 import { createMaterialBottomTabNavigator } from "react-navigation-material-bottom-tabs";
+import { createDrawerNavigator } from "react-navigation-drawer";
 
 import CategoriesScreen from "../screens/CategoriesScreen";
 import CategoryMealsScreen from "../screens/CategoryMealsScreen";
 import MealDetailScreen from "../screens/MealDetailScreen";
+import FiltersScreen from "../screens/FiltersScreen";
 import Colors from "../constants/colors";
 import FavoritesScreen from "../screens/FavoritesScreen";
 
@@ -86,4 +88,13 @@ const MealsFavTabNavigator =
         },
       });
 
-export default createAppContainer(MealsFavTabNavigator);
+const FiltersNavigator = createStackNavigator({
+  Filters: FiltersScreen,
+});
+
+const MainNavigator = createDrawerNavigator({
+  MealsFavs: MealsFavTabNavigator,
+  Filters: FiltersNavigator,
+});
+
+export default createAppContainer(MainNavigator);
